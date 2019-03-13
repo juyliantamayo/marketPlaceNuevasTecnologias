df939f33a55893613db3cc965408e1a092bad3eb
julian tamayo <juliantamayo16@outlook.es>
2019-03-12 23:50:07 -0500

    se termino la clase productologic

--- MarketPlace/src/main/java/co/edu/konrad/marketplace/logic/productoLogic.java
+++ MarketPlace/src/main/java/co/edu/konrad/marketplace/logic/productoLogic.java
@@ -18,25 +18,34 @@ import javax.inject.Inject;
  */
 @Stateless
 public class productoLogic {
+
     @Inject
     private productoPersistence persistence;
     
-    public List<productoEntity> obtenerProductos(){
-      List<productoEntity> productos =   persistence.encontrarTodos();
-      return productos;
+    public List<productoEntity> obtenerProductos() {
+        List<productoEntity> productos = persistence.encontrarTodos();
+        return productos;
     }
-    public productoEntity obtenerProducto(Long Id) throws IllegalAccessException{
-       
-        
-        
-    productoEntity producto = persistence.encontrarPorId(Id);
-        if (producto==null) {
-            throw new IllegalAccessException("el problema");
+    
+    public productoEntity obtenerProducto(Long Id) throws IllegalAccessException {
+        productoEntity producto = persistence.encontrarPorId(Id);
+        if (producto == null) {
+            throw new IllegalAccessException("el producto seleccionado no existe");
         }
         return producto;
     }
-    public void insertarProducto(){
-    
+
+    public productoEntity crearProducto(productoEntity producto) {
+        persistence.inserta(producto);
+        return producto;
     }
 
+    public productoEntity actualizarProducto(Long id, productoEntity producto) {
+        productoEntity productoActualizar = persistence.actualizar(producto);
+        return productoActualizar;
+    }
+
+    public void eliminarProducto(Long id) {
+        persistence.eliminar(id);
+    }
 }
